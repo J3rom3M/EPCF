8259f00404f08ceffb069f32bf38db86431922a2
J3rom3M <craozor@gmail.com>
2022-01-25 17:09:28 +0100

    mardi 25/01/2022 EPCF Jérôme Magnier CDA1021

---
+++ src/data/peoples-ranking.js
@@ -0,0 +1,25 @@
+export default [{
+  flag: 'https://cdn-icons-png.flaticon.com/512/323/323315.png',
+  name: 'France',
+  peoplesCount: 65000000
+}, {
+  flag: 'https://cdn-icons-png.flaticon.com/512/197/197571.png',
+  name: 'Germany',
+  peoplesCount: 85000000
+}, {
+  flag: 'https://cdn-icons-png.flaticon.com/512/197/197593.png',
+  name: 'Spanish',
+  peoplesCount: 127000000
+}, {
+  flag: 'https://cdn-icons-png.flaticon.com/512/323/323325.png',
+  name: 'Italy',
+  peoplesCount: 35000000
+}, {
+  flag: 'https://cdn-icons-png.flaticon.com/512/323/323325.png',
+  name: 'Italy',
+  peoplesCount: 35000000
+}, {
+  flag: 'https://cdn-icons-png.flaticon.com/512/197/197571.png',
+  name: 'Germany',
+  peoplesCount: 85000000
+}];
